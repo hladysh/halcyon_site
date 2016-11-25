3b7d0785a73595cfbdd36f69aa84cd716e197deb
Ivan <hladysh.im@gmail.com>
2016-11-25 19:33:13 +0000

    done navigation

--- script/main.js
+++ script/main.js
@@ -11,14 +11,6 @@ $(window).load(function() {
 		pauseOnHover: true,
 	});
 
-	$('#servicesSlider').flexslider({
-		animation: "slide",
-		directionNav: true,
-		controlNav: true,
-		touch: true,
-		pauseOnHover: true,
-	});
-
 	$('#teamSlider').flexslider({
 		animation: "slide",
 		directionNav: false,
@@ -27,6 +19,38 @@ $(window).load(function() {
 		pauseOnHover: true,
 	});
 
-});
+
 
 /***************** Flexsliders  end ******************/
+
+
+/***************** Slide-In Nav ******************/
+
+	$('.nav_slide_button').click(function() {
+		$('.pull').slideToggle();
+	});
+	$( "#nav-toggle" ).click(function() {
+ 		 $( this ).toggleClass( "active" );
+});
+
+/***************** Smooth Scrolling ******************/
+
+$(function() {
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
+		}
+	});
+
+});
+
+});
